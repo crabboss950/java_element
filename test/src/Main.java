54395cbae1d21909642f0732571269c9ce075e0f
crabboss950 <silverbell950@naver.com>
2021-11-25 17:12:52 +0900

    s

--- test/src/Main.java
+++ test/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("¾È³çÇÏ¼¼¿ä");
+		System.out.println("ì•ˆë…• ì¹œêµ¬ë“¤");
 	}
 
 }
